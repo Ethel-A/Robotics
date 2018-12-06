8f2e6bc3e4de150c14b1010c4c5bbcb63ceb51e3
Ethel-A <etheladongo98@gmail.com>
2018-12-06 00:01:06 +0000

    Tests for Movement Library

---
+++ tests.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+from movement.ev3movement.movement import *
+
+move_straight_line(150, 500)
+turn_right_by_angle(120, 500)
+move_straight_line(180, 500)
+turn_right_by_angle(150, 500)
+move_straight_line(130, 500)
+turn_right_by_angle(90, 500)
+#move_straight_line(50, 500)
+
+#turn_right_by_angle(90, 450)
+
+#turn_left_by_angle(90, 450)
+
+#spin_left_by_angle(180, 450)
+
+#spin_right_by_angle(90, 450)
